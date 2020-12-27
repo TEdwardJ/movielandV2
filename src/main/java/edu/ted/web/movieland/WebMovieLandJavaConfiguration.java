3335c22ade88cc8032c2799d8243d82b9e3b3801
TedwardJ <edt77@ukr.net>
2020-12-27 18:49:16 +0200

    Movie controller is created:  - controller class named MovieController is created;  - Web Configuration has got the appropriate @ComponentScan annotation to find the controller;

--- src/main/java/edu/ted/web/movieland/WebMovieLandJavaConfiguration.java
+++ src/main/java/edu/ted/web/movieland/WebMovieLandJavaConfiguration.java
@@ -1,7 +1,10 @@
 package edu.ted.web.movieland;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"edu.ted.web.movieland.controller"})
 public class WebMovieLandJavaConfiguration {
+
 }
---
+++ src/main/java/edu/ted/web/movieland/controller/MovieController.java
@@ -0,0 +1,9 @@
+package edu.ted.web.movieland.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1")
+public class MovieController {
+}
