d0b0152677b6548db96aae35b2a824ace570015d
TedwardJ <edt77@ukr.net>
2020-12-27 18:45:45 +0200

    MovieLandV2 configuration is created:  - draft classes for root and web java configurations added;  - initializer class added for root and web java configurations;

---
+++ src/main/java/edu/ted/web/movieland/MovieLandAnnotationConfigDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package edu.ted.web.movieland;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MovieLandAnnotationConfigDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{MovieLandJavaConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebMovieLandJavaConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/*" };
+    }
+}
---
+++ src/main/java/edu/ted/web/movieland/WebMovieLandJavaConfiguration.java
@@ -0,0 +1,7 @@
+package edu.ted.web.movieland;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebMovieLandJavaConfiguration {
+}
