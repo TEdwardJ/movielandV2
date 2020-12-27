5b39a2f67cfb9f81bb8839cf9e6d1f9492d91219
TedwardJ <edt77@ukr.net>
2020-12-27 19:44:37 +0200

    Movie controller made formally working:  - spring-test added as dependency into pom.xml;  - MovieController test added;  - WebMovieLandJavaConfiguration has got @EnableWebMvc annotation;

---
+++ src/test/java/edu/ted/web/movieland/controller/MovieControllerITest.java
@@ -0,0 +1,39 @@
+package edu.ted.web.movieland.controller;
+
+
+import edu.ted.web.movieland.MovieLandJavaConfiguration;
+import edu.ted.web.movieland.WebMovieLandJavaConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration("")
+@ContextConfiguration(classes={MovieLandJavaConfiguration.class, WebMovieLandJavaConfiguration.class})
+public class MovieControllerITest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void test() throws Exception {
+        mockMvc.perform(get("/v1/movie"))
+                .andExpect(status().isOk());
+    }
+}
